a8140b384dd91c7183eb63aced75f1c9a3c89415
pyarelal-mountblue <pyarelal.sahu.17@mountblue.tech>
2021-08-18 12:15:33 +0530

    modified counterFactory.js

--- counterFactory.js
+++ counterFactory.js
@@ -1,14 +1,12 @@
 function counterFactory() {
     let counter = 0;
     function increment(numberOfIncrements) {
-        for (let i = 0; i < numberOfIncrements; i++)
-            counter += 1;
+        counter += numberOfIncrements;
         return counter;
     }
 
     function decrement(numberOfDecrements) {
-        for (let i = 0; i < numberOfDecrements; i++)
-            counter -= 1;
+        counter -= numberOfDecrements;
         return counter;
     }
 
